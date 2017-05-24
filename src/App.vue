8ccff47b62d6c64f7a6a2f22808ab9910b7d47e6
juanzi <1532490300@qq.com>
2017-05-24 14:29:26 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <!-- <img src="./assets/logo.png"> -->
+    <div class="marquee-wrap"><vue-marquee content="222222222" class="two"  showtwo="false"></vue-marquee></div>
+    <div class="marquee-wrap"><vue-marquee content="111111111" class="two"  showtwo="false"></vue-marquee></div>
+    <div class="marquee-wrap"><vue-marquee content="3" class="two"  :showtwo="false" speed="quick" ></vue-marquee></div>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+// import Hello from './components/Hello'
+import vueMarquee from './Marquee.vue'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    vueMarquee
   }
 }
 </script>
@@ -24,5 +27,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.marquee-wrap {
+  width: 100px;
+  margin: 0 auto;
 }
 </style>
