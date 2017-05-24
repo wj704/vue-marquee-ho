7aa18c796e78b61f8e79deb0b401bcc462bf1e00
juanzi <1532490300@qq.com>
2017-05-24 14:27:10 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,18 +1,19 @@
-// // The Vue build version to load with the `import` command
-// // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 // import Vue from 'vue'
 // import App from './App'
 
-// Vue.config.productionTip = false
+// // Vue.config.productionTip = false
 
-// /* eslint-disable no-new */
+//  // eslint-disable no-new
 // new Vue({
 //   el: '#app',
 //   template: '<App/>',
 //   components: { App }
-// })
+// });
 
-import Marquee from './Marquee.vue'
+import VueMarquee from './Marquee.vue'
+export default VueMarquee;
 
-
-module.exports = Marquee
+// var VueMarquee = require('./Marquee.vue');
+// module.exports = VueMarquee
